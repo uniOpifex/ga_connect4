abb3e7966fefc9fe5dd352abc8cd3983eff699ea
uniOpifex <njhunit@gmail.com>
2017-09-18 00:37:18 -0400

    updated html and scrs

---
+++ src/script.js
@@ -0,0 +1,14 @@
+
+
+/*- startGame-
+    --game starts on red
+    -player selects a column and game and token
+    --game checks for win
+
+
+    -- Win State
+        check for horizontal, vertical, and diagnol win.
+        clears board. -
+        
+*/
+
