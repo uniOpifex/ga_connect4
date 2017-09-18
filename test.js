682587d28ba14a7ae21698e776e51c52f2f563fa
uniOpifex <njhunit@gmail.com>
2017-09-18 04:25:19 -0400

    final logic in progress --many bugs

--- test.js
+++ test.js
@@ -11,3 +11,42 @@
         clears board. -
         
 */
+function checkForWin(columns) {
+ //check for 4 in a row
+ for(var i=0;i<5;i++)
+ for(var j=0;j<7;j++)
+  if(board[i][j]!=0&&board[i][j]==board[i+1][j]&&
+  board[i][j]==board[i+2][j]&&board[i][j]==board[i+3][j])
+   if(board[i][j]==1)
+    return 100;
+   else return -100;
+    
+//check for 4 in a column
+for(var i=0;i<8;i++)
+ for(var j=0;j<4;j++)
+  if(board[i][j]!=0&&board[i][j]==board[i][j+1]&&
+  board[i][j]==board[i][j+2]&&board[i][j]==board[i][j+3])
+   if(board[i][j]==1)
+    return 100;
+   else return -100;
+    
+//check for 4 in a diagonal
+for(var i=0;i<5;i++)
+ for(var j=0;j<4;j++)
+  if(board[i][j]!=0&&board[i][j]==board[i+1][j+1]&&
+  board[i][j]==board[i+2][j+2]&&board[i][j]==board[i+3][j+3])
+   if(board[i][j]==1)
+    return 100;
+   else return -100;
+   
+//check for 4 in the reverse diagonal
+for(var i=3;i<8;i++)
+ for(var j=0;j<4;j++)
+  if(board[i][j]!=0&&board[i][j]==board[i-1][j+1]&&
+  board[i][j]==board[i-2][j+2]&&board[i][j]==board[i-3][j+3])
+   if(board[i][j]==1)
+    return 100;
+   else return -100;
+return 0;
+}
+}
