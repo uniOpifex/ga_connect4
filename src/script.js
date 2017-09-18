ec6eafb44fd655021876c1bc789366e3c95091f9
uniOpifex <njhunit@gmail.com>
2017-09-18 00:55:02 -0400

    more logic

--- src/script.js
+++ src/script.js
@@ -12,9 +12,31 @@
         
 */
 
-var counter= 1;
-var things = '#' ;
-$('.gameCol').click(function() { 
-  $("#"+counter).css("background-color", "red")
-  counter++;
-})
+$(".arow").click(function(){
+    var col = $(this).index();
+  console.log(col)
+    
+    
+    var activePlayer = $(this).closest(".gameBoard").data("activeplayer"); 
+    //Get First available slot
+    var cells = $(" .gameBoard .arow div:nth-child(" + (col+1)  +")");
+    
+    var cell;
+    for(var i = cells.length -1; i > -1; i--)
+    {
+        if($(cells[i]).data("token") == undefined)
+        {
+            cell = cells[i];
+            break;
+        }
+    }
+    
+    $(cell).data("token", activePlayer).addClass(activePlayer);      
+   
+    
+    //Toggle Active Player 
+    activePlayer = activePlayer == "red" ? "blue" : "red";
+    $(this).closest(".gameBoard").toggleClass("red blue").data("activeplayer", activePlayer);
+    
+
+});
